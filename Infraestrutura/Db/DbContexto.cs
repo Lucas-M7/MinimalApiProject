f2f0c9f3e4406e79c9e059cdd56c08b2f1661fb3
Lucas Mateus <Lucas015x@gmail.com>
2024-03-07 19:26:49 -0300

    Adicionando a nova migration

--- Infraestrutura/Db/DbContexto.cs
+++ Infraestrutura/Db/DbContexto.cs
@@ -5,6 +5,20 @@ namespace MinimalApi.Infraestutura.Db;
 
 public class DbContexto : DbContext
 {
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Administrador>().HasData(
+            new Administrador
+            {
+                Id = 1,
+                Email = "adm@teste.com",
+                Senha = "1234567",
+                Perfil = "Adm"
+            }
+        );
+    }
+
     public DbContexto(DbContextOptions<DbContexto> options) : base(options)
     {
     }
